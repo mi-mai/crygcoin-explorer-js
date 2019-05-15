73c0d289a3f314deed8e7fab60a3b7f2a1f20dd2
mi-mai <mimainer777@gmail.com>
2019-05-15 08:20:02 +0300

    v.0.0.2
    update icon

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var api = 'http://127.0.0.1:18898';
 var donationAddress = "";
 var blockTargetInterval = 30; // enter the block interval in seconds
 var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
-var totalSupply =  100000000000; // enter the total supply in atomic units
+var totalSupply =  10000000000000; // enter the total supply in atomic units
 var symbol = 'cryg'; // enter the coin's ticker
 var refreshDelay = 30000;
 
@@ -10,6 +10,7 @@ var refreshDelay = 30000;
 var networkStat = {
  "cryg": [
 	["pool.cryg.xyz", "http://pool.cryg.xyz:8117"],
+	["94.199.76.165", "http://94.199.76.165:8117"],
  ]
 };
 
